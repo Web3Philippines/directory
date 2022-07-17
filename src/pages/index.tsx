22291b66146d9ecc265e04e1b2395b5bec3fa4c2
John Carl James Nebab <nebab.johncarl@gmail.com>
2022-07-17 22:24:43 +0800

    ☕ CHORE: removed unnecessary css file and converted styles to tw classes in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,25 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 import Seo from "@/components/Seo";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="px-0 py-8">
       <Seo templateTitle="Home" />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://web3philippines.org">Web3 Philippines Directory!</a>
+      <main className="flex min-h-screen flex-1 flex-col justify-center py-16 px-6 text-center lg:px-0">
+        <h1 className="m-0 text-5xl leading-[1.15] md:text-[4rem]">
+          Welcome to{" "}
+          <a
+            href="https://web3philippines.org"
+            className="text-[#0070f3] hover:underline focus:underline active:underline"
+          >
+            Web3 Philippines Directory!
+          </a>
         </h1>
-        <p className={styles.description}>Currently, under development! 🙏</p>
+        <p className="my-[4rem] mx-0 text-center text-2xl leading-[1.5]">
+          Currently, under development! 🙏
+        </p>
       </main>
-
     </div>
   );
 };

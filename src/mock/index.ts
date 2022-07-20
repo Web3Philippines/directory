2e2b114767981b673f7c83e69cb3882d10b71bac
Jianne Gabriel <jianne.gabriel@twala.io>
2022-07-20 14:35:34 +0800

    ☕ CHORE: add mock data

---
+++ src/mock/index.ts
@@ -0,0 +1,42 @@
+interface Project {
+  id: string;
+  name: string;
+  verified: boolean;
+  description: string;
+  tags: Array<string>;
+  links: {
+    facebook: {
+      id: string;
+      url: string;
+    };
+    discord: {
+      id: string;
+      url: string;
+    };
+    twitter?: {
+      id: string;
+      url: string;
+    };
+  };
+}
+
+export const projects: Array<Project> = [
+  {
+    id: "web3ph",
+    name: "Web3 PHL",
+    verified: true,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus ipsam qui excepturi quam eius, ratione nulla, nesciunt quo autem consequatur rem debitis, atque adipisci saepe est sed porro? Illum, incidunt.",
+    tags: ["nft", "defi", "gaming"],
+    links: {
+      facebook: {
+        id: "fb",
+        url: "https://web.facebook.com/groups/web3philippines",
+      },
+      discord: {
+        id: "discord",
+        url: "https://discord.com/invite/4xbJEBKWKc",
+      },
+    },
+  },
+];

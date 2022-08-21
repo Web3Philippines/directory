f0e5db73bdf3d64304b0cdbcd99fda4fa65a21ad
Waren Gonzaga <github@warengonzaga.com>
2022-08-21 12:49:07 +0800

    🐛 FIX: capitalize site name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,8 +23,8 @@ const Home: NextPage = () => {
       <Seo templateTitle="Home" />
       <header className="min-w-screen w-full bg-header-pattern bg-cover bg-bottom md:h-[160px] md:rounded-bl-[100px]">
         <div className="mx-auto flex h-[200px] max-w-screen-2xl flex-col items-center justify-between px-6 md:h-[160px] md:flex-row md:items-center md:rounded-bl-[100px] md:px-10 lg:px-[165px] xl:px-[100px]">
-          <h3 className="mt-6 text-3xl font-bold lowercase text-white md:mt-0">
-            Web3 Philippines Directory
+          <h3 className="mt-6 text-3xl font-bold capitalize text-white md:mt-0">
+            Web3 Philippines Directory 📃
           </h3>
           <a
             href="https://forms.gle/8BUfE2A7NRtqYbm66"

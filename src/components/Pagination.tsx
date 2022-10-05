a042814fcb20f96f7c1251cf6f8b0e025f2ebff0
jnebab.eth <nebab.johncarl@gmail.com>
2022-10-05 23:52:14 +0800

    Update src/components/Pagination.tsx

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -33,7 +33,7 @@ function Pagination({
           <span>Prev</span>
         </button>
         <button
-          className=" flex flex-row items-center space-x-3 rounded-r-md border-l border-white bg-gray-700 py-2 px-4 text-white hover:bg-gray-600 disabled:cursor-not-allowed disabled:bg-gray-500"
+          className=" flex flex-row items-center space-x-3 rounded-r-md border-l border-white bg-gradient-to-r from-purple-900 to-violet-500 py-2 px-4 text-white hover:bg-gray-600 disabled:cursor-not-allowed disabled:bg-gray-500"
           onClick={() => setPage(page + 1)}
           disabled={!hasNextPage}
         >
